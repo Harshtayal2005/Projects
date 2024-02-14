b14458b52042c5b7bab2767cd18bf299d768a579
Harsh Tayal <harshtayal2005@gmail.com>
2024-02-14 15:35:02 +0530

    modified

--- Projects/to-do-app/src/components/Items.jsx
+++ Projects/to-do-app/src/components/Items.jsx
@@ -4,7 +4,7 @@ function Items({ passItems, onDeleteClick }) {
   return (
     <div className="grid grid-cols-6 gap-4 px-5 my-4">
       {passItems.map((items) => (
-        <Item items={items} deleteItem={onDeleteClick}></Item>
+        <Item items={items} deleteItem={onDeleteClick} key={items}></Item>
       ))}
     </div>
   );
--- Projects/to-do-app/src/components/TakeInput.jsx
+++ Projects/to-do-app/src/components/TakeInput.jsx
@@ -18,6 +18,19 @@ function TakeInput({ onNewItem }) {
     setTodoDateChange("");
   };
 
+  const handleKeyDown = ({ key }) => {
+    if (key === "Enter") {
+      onNewItem(todoNameChange, todoDateChange);
+    }
+  };
+
+  const handleKeyDown2 = (evt) => {
+    evt.preventDefault();
+    if (evt.key === "Enter") {
+      onNewItem(todoNameChange, todoDateChange);
+    }
+  };
+
   return (
     <div className="grid grid-cols-6 gap-4 px-5 my-4">
       <input
@@ -26,12 +39,14 @@ function TakeInput({ onNewItem }) {
         placeholder="Enter Todo here"
         value={todoNameChange}
         onChange={handleNameChange}
+        onKeyDown={handleKeyDown}
       />
       <input
-        className="col-span-2 border border-gray-400 rounded-md px-2 h-[3rem] font-serif"
+        className="col-span-2 border border-gray-400 rounded-md px-2 h-[3rem] font-serif "
         type="date"
         value={todoDateChange}
         onChange={handleDateChange}
+        onKeyDown={handleKeyDown2}
       />
       <Button
         className="col-span-1 bg-green-800 h-[3rem]"
